5a68865a009c5afc0240afb03423965c402811e6
tyagi0320 <127464668+tyagi0320@users.noreply.github.com>
2024-02-21 14:01:28 +0000

    conversions and operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,71 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let val= Number(score)
+
+//NaN: not a number
+
+console.log(typeof val)
+console.log(val)
+
+// "33" => 33
+// "33abc" => NaN but its type is number
+// true => 1,false => 0
+
+let isloggedIn =1
+
+let booleanisLoggedIn = Boolean(isloggedIn)
+console.log(booleanisLoggedIn)
+
+//1 => true ; 0 => false
+//"" => false
+//"harshit" => true
+
+let alpha=3
+let numtostring=String(alpha)
+console.log(typeof numtostring)
+
+/*  operations */
+
+let value =3
+let negvalue = -value
+console.log(negvalue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2/2)
+//console.log(2*2)
+//console.log(2^2)
+//console.log(2%2)
+
+let str1="hello"
+let str2=" harshit"
+
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+"2")
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+3)
+console.log(1+2+"3")
+
+//console.log(+true) => 1
+//console.log(true+) => error
+//console.log(+"") => 0
+
+let n1,n2,n3
+n1=n2=n3 =2+2
+console.log(n1)
+
+let cnt1=100
+let cnt2=100
+cnt1++
+console.log(cnt1)
+++cnt2
+console.log(cnt2)
+++cnt2
+
+
+
