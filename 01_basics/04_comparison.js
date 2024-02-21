1726576a2f8e368c39a8f6f6f3716fb7812dc74f
tyagi0320 <127464668+tyagi0320@users.noreply.github.com>
2024-02-21 14:15:46 +0000

    coparing datatypes using various operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+//console.log("2">1) => true (converts "2" to number)
+
+//console.log(null>0) => false
+//console.log(null==0) => false
+//console.log(null>=0) => true
+
+/*comparisons convert null to a number treating it as 0
+that is why null>=0 is true and nul>0 is false*/
+
+//console.log(undefined>0) => false
+//console.log(undefined==0) => false
+//console.log(undefined>=0) => false
+
+// strict check ====
+console.log("2"===2)
