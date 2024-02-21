891de3e91d94676ef377ab19fd33969fb28c0de2
tyagi0320 <127464668+tyagi0320@users.noreply.github.com>
2024-02-21 13:38:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all jscode as newer version
+
+//alert(3+3) //works in browser not nodejs
+
+let name="124" //string
+let age=18 //number
+let islogin=false //bool
+
+//number => 2^53
+//bigint => greater than 2^53
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof(undefined))
