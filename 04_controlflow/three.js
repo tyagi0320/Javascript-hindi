f975a3152b7331f95534aeb789fe8321014bef3a
tyagi0320 <127464668+tyagi0320@users.noreply.github.com>
2024-02-26 12:12:12 +0000

    to be done....

---
+++ 04_controlflow/three.js
@@ -0,0 +1,13 @@
+//for of
+
+const arr=[1,20,3,40,5]
+for (const i of arr) {
+    console.log(i)  
+}
+
+const greetings="Hello world"
+for(const i of greetings){
+    console.log(`Each char is ${i}`)
+}
+
+//
