2e579b2f9aac8368c72f9c4c87ec129a41fb3945
tyagi0320 <127464668+tyagi0320@users.noreply.github.com>
2024-02-26 11:50:20 +0000

    if-else-switch-terniary..etc

---
+++ 04_controlflow/one.js
@@ -0,0 +1,40 @@
+//if
+//else-if
+//falsy values
+//false,0,-0,BigInt 0n,"",null,undefined,Nan
+//truthy values
+//"0","false"," ",[],{},function(){},
+//switch
+/* 
+const month = 3
+switch(month){
+    case 1:
+        console.log("jan")
+        break;
+    case:2
+        console.log("feb")
+        break;
+     case 3:
+        console.log("march")
+        break;
+    default:
+        break;
+}
+*/
+
+//Nullish Coalescing Operator (??): null undefined
+/*
+let val1;
+val1=5 ?? 10
+val1=null ?? 13
+val1=undefined ?? 11
+val1=null ?? 10 ?? 20
+console.log(val1)
+*/
+
+//Terniary Operator
+// condition ? true:false
+/*
+const price=100
+price>=80 ? console.log("More than 80") : console.log("Less than 80")
+*/
