00d7d587995dd13f1ecfe206fd09844c51485b69
tyagi0320 <127464668+tyagi0320@users.noreply.github.com>
2024-03-08 17:30:56 +0000

    Fetch() working

--- 09_advance_one/promises_02.js
+++ 09_advance_one/promises_02.js
@@ -1,1 +1,12 @@
 //UNDERSTANDING FETCH() WORKING
+
+//response=fetch('something'):
+
+//When we use fetch() two events takes place.
+//1.Reservation of datascpaces for variables/memory.
+// a.onFulfilled[]=={similar to resolve of promise}
+// b.onRejection[]=={similar to reject of promise}
+//2.Handling of browser/node APIs.
+//You get a network request from here
+// a.If data goes on network request and gets some respone: request goes to onFullfilled[]{or resolve}
+// b.If data do not go on network request and response is stuck: request goes to onRejection[]{or reject}
